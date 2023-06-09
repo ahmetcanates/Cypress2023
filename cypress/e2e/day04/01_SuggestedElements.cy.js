135af4e266de1189caa82e1306d6a9d7ad17917e
Ahmet Can Ates <omerzed54@gmail.com>
2023-06-09 21:20:29 +0300

    v2

---
+++ cypress/e2e/day04/01_SuggestedElements.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Suggested Elements',() => {
+
+    it('test case', () => {
+        cy.visit('http://automationpractice.pl/index.php')
+
+        cy.get('#search_query_top').type('dress')
+
+        cy.wait(2000)
+
+        cy.get("div.ac_results").contains('Blouses').click()    
+
+        cy.get('h1').contains('Blouse').should('be.visible')
+
+    })
+
+  
+
+
+})
