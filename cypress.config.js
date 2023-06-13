dc175d8d96a8a891f5ca7fa30e4b5b4c55759c7f
Ahmet Can Ates <omerzed54@gmail.com>
2023-06-13 21:19:20 +0300

    v1

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl : "https://www.google.com"
+    baseUrl : "https://www.google.com",
+    "video":false,
+    //testlerin videolarini kaydetmiyor
+    //"video":true testlerin videolarini kaydeder
+    //default olarak "video":true 'dur.
+
+    "retries":2
+    //TEST FAIL OLDUGUNDA onu kac kere daha calistirmasi gerektigini belirtiyoruz
+    //"retries":2 => 1 kere calistirip hata aldiginda 2 kere daha dene
   },
 });
