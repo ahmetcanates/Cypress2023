6f2b5fe226f3135c1d3a04116a117cf31203baa1
Ahmet Can Ates <omerzed54@gmail.com>
2023-06-14 02:42:45 +0300

    v3

--- cypress/e2e/day07/05_ReadFile&Assert.cy.js
+++ cypress/e2e/day07/05_ReadFile&Assert.cy.js
@@ -2,8 +2,14 @@
 
 describe('Read File And Assert',() => {
 
-    it('', () => {
+    it.only('Assert', () => {
 
+        cy.readFile('./cypress/fixtures/users.json').then((userdata) => {
+            expect(userdata[3].name).equal('Patricia Lebsack')
+            expect(userdata[3].username).equal('Karianne')
+            expect(userdata[3].address.street).equal('Hoeger Mall')
+            expect(userdata[3].company.name).equal('Robel-Corkery')
+        })
 
     })
 
