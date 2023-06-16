75c287d15f5c1339491b2a6d6313d5c0eaecdffc
Ahmet Can Ates <omerzed54@gmail.com>
2023-06-17 01:25:36 +0300

    v2

--- cypress/e2e/day09/07_DELETE.cy.js
+++ cypress/e2e/day09/07_DELETE.cy.js
@@ -1,19 +1,17 @@
 /// <reference types="cypress" />
 
-describe('POST 02',() => {
+describe('DELETE',() => {
 
     it('TEST', () => {
-        const bodyRequest = {"name":"Cypress","salary":"500","age":"21"}
 
         cy.request({
-            url: 'https://dummy.restapiexample.com/api/v1/create',
-            method: 'POST',
-            body: bodyRequest})
+            url: 'https://dummy.restapiexample.com/api/v1/delete/2',
+            method: 'DELETE'})
             .then((response) => {
-                expect(response.status).eq(200)
-                expect(response.body.status).eq('success')
-                expect(response.body.message).eq('Successfully! Record has been added.')
+                expect(response.body.status).eq("success")
+                expect(response.body.message).eq("Successfully! Record has been deleted")
             })
+
     })
 
  
